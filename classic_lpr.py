cc0fe79ef9a30ede323e4a6547879a155bce92ff
eMrazSVK <eduardmraz2@gmail.com>
2018-04-29 14:54:43 +0200

    add OCR using KNearest

--- classic_lpr.py
+++ classic_lpr.py
@@ -107,27 +107,129 @@ def detect_plate_candidates(input_image, show_processing, num):
 # do OCR within ROI's in input image (ROI's gathered from detect_plate_candidates()), return characters recognized
 # also do some processing within ROI (threshold)
 def plate_candidates_ocr():
-
     global read_count
     # apply mean-shift-filter, slow, but helps a lot
     # plate_candidates = cv2.pyrMeanShiftFiltering(plate_candidates,60.0,60.0)
+
+    samples = np.loadtxt('char_samples.data', np.float32)
+    responses = np.loadtxt('char_responses.data', np.float32)
+    responses = responses.reshape((responses.size, 1))
+
+    model = cv2.ml.KNearest_create()
+    model.train(samples, cv2.ml.ROW_SAMPLE, responses)
 
     while True:
         print(str(read_count))
         if read_count < write_count:
             for i in range(0, write_count-read_count):
                 plate_candidate = cv2.imread(IMAGE_PATH+str(read_count)+'.jpg', 1)
-                plate_candidate = cv2.cvtColor(plate_candidate, cv2.COLOR_BGR2GRAY)
-                plate_candidate = cv2.threshold(plate_candidate, 110, 255, cv2.THRESH_BINARY)[1]
-                detected_text = pytesseract.image_to_string(plate_candidate)
+                img = clean_image(plate_candidate)
+                clean_img, chars = extract_characters(img)
+                plate_chars = ""
                 read_count += 1
+
+                for bbox, char_img in chars:
+                    width, height = char_img.shape
+                    if width is not 0 and height is not 0:
+                        small_img = cv2.resize(char_img, (10, 10))
+                        small_img = small_img.reshape((1, 100))
+                        small_img = np.float32(small_img)
+                        retval, results, neigh_resp, dists = model.findNearest(small_img, k=1)
+                        plate_chars += str(chr((results[0][0])))
                 
-                if detected_text is not None:
+                if plate_chars is not "":
                     original_image = cv2.imread(FINAL_PATH + str(read_count) + '.jpg', 1)
-                    cv2.putText(original_image, detected_text,(50,50), font, fontScale, fontColor, lineType)
+                    cv2.putText(original_image, plate_chars,(50,50), font, fontScale, fontColor, lineType)
                     cv2.imwrite(FINAL_PATH + str(read_count) + '.jpg', original_image)
                 else:
                     pass
+
+
+def reduce_colors(img, n):
+    Z = img.reshape((-1,3))
+
+    # convert to np.float32
+    Z = np.float32(Z)
+
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = n
+    ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+    # Now convert back into uint8, and make original image
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape((img.shape))
+
+    return res2
+
+
+def clean_image(img):
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    resized_img = cv2.resize(gray_img
+        , None
+        , fx=5.0
+        , fy=5.0
+        , interpolation=cv2.INTER_CUBIC)
+
+    resized_img = cv2.GaussianBlur(resized_img,(5,5),0)
+    cv2.imwrite('licence_plate_large.png', resized_img)
+
+    equalized_img = cv2.equalizeHist(resized_img)
+    cv2.imwrite('licence_plate_equ.png', equalized_img)
+
+
+    reduced = cv2.cvtColor(reduce_colors(cv2.cvtColor(equalized_img, cv2.COLOR_GRAY2BGR), 8), cv2.COLOR_BGR2GRAY)
+    cv2.imwrite('licence_plate_red.png', reduced)
+
+
+    ret, mask = cv2.threshold(reduced, 80, 255, cv2.THRESH_BINARY)
+    cv2.imwrite('licence_plate_mask.png', mask)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    mask = cv2.erode(mask, kernel, iterations = 1)
+    cv2.imwrite('licence_plate_mask2.png', mask)
+
+    return mask
+
+
+def extract_characters(img):
+    bw_image = cv2.bitwise_not(img)
+    contours = cv2.findContours(bw_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
+
+    char_mask = np.zeros_like(img)
+    bounding_boxes = []
+    for contour in contours:
+        x,y,w,h = cv2.boundingRect(contour)
+        area = w * h
+        center = (x + w/2, y + h/2)
+        if (area > 1000) and (area < 10000) and (h > 60):
+            x,y,w,h = x-4, y-4, w+8, h+8
+            bounding_boxes.append((center, (x,y,w,h)))
+            cv2.rectangle(char_mask,(x,y),(x+w,y+h),255,-1)
+
+    cv2.imwrite('licence_plate_mask3.png', char_mask)
+
+    clean = cv2.bitwise_not(cv2.bitwise_and(char_mask, char_mask, mask = bw_image))
+
+    bounding_boxes = sorted(bounding_boxes, key=lambda item: item[0][0])
+
+    characters = []
+    for center, bbox in bounding_boxes:
+        x,y,w,h = bbox
+        char_image = clean[y:y+h,x:x+w]
+        characters.append((bbox, char_image))
+
+    return clean, characters
+
+
+def highlight_characters(img, chars):
+    output_img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    for bbox, char_img in chars:
+        x,y,w,h = bbox
+        cv2.rectangle(output_img,(x,y),(x+w,y+h),255,1)
+
+    return output_img
 
 
 # main function
@@ -139,6 +241,7 @@ def main():
         ret, frame = cap.read()
         width, height, channels = frame.shape
         frame = cv2.resize(frame, (int(0.4 * height), int(0.4 * width)))
+
 
         if counter % 1 == 0:
             detect_plate_candidates(frame, True, counter)
